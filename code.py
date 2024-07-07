82dae62f95792d9311dbeb866cfadf4af1dc477c
Vladimir Mikryukov <vmikryukov@gmail.com>
2024-07-07 19:09:55 +0300

    upd

--- code.py
+++ code.py
@@ -2,14 +2,15 @@
 import time
 import board
 import neopixel
+import random
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
 # Built-in NeoPixel setup
-n = 1  # Number of NeoPixels
-pin = board.GP16  # Pin where NeoPixel is connected
+n = 1                          # Number of NeoPixels
+pin = board.GP16               # Pin where NeoPixel is connected
 np = neopixel.NeoPixel(pin, n)
 
 # HID keyboard setup
